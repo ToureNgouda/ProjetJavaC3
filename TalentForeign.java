edf3e7306d1ad5857441fdeb2a73cbb972d24bc2
Ngouda Toure <ngoudafall@yahoo.com>
2018-01-08 04:46:11 +0100

    modif3

--- TalentForeign.java
+++ TalentForeign.java
@@ -4,13 +4,20 @@ public class TalentForeign extends Talent{
 	
    private String  countryOrigin;
 	
+   
+   /*constructor with 6parameters in the following order
+	* nameTalent, firstNameTalent, ageTalent, levelTalent, specializationCoursetalent, countryOriginTalent
+	* call the constructor of the Talent class
+     * initializes the attributes of the TalentForeign class 
+    */
    public TalentForeign(String nameTalent, String fisrtNameTalent, int ageTalent, String levelTalent,
 			String specializationCourseTalent, String countryOriginTalent) {
 		super(nameTalent, fisrtNameTalent, ageTalent, levelTalent, specializationCourseTalent);
 		countryOrigin =  countryOriginTalent;
 	}
 
-
+    //method that display identity of a TalentForeign
+   //call the method print() of the Talent class
     public String print() {
     	return super.print()+ "," +countryOrigin;
     }
