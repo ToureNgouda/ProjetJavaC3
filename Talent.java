169e3da1988ebe3d587edecf6913f7a2f98c533e
Ngouda Toure <ngoudafall@yahoo.com>
2018-01-08 03:25:20 +0100

    second commit

---
+++ Talent.java
@@ -0,0 +1,47 @@
+package projetJavaC3.gestionDesTalents;
+
+public class Talent {
+	
+	   private  String  name;
+	   private  String  fisrtName;
+	   private  int     age;
+	   private  String  level;
+	   private  String  specializationCourse;
+	   
+	   
+	   public Talent(String nameTalent, String fisrtNameTalent, int ageTalent, String levelTalent, String specializationCourseTalent) {
+		   
+		   name = nameTalent;
+		   fisrtName = fisrtNameTalent;
+		   age = ageTalent;
+		   level = levelTalent;
+		   specializationCourse = specializationCourseTalent;	
+		  
+	   }
+
+
+		public String getName() {
+			return name;
+		  }
+		   
+	      public String getFisrtName() {
+		    return fisrtName;
+	      }
+
+	      public int getAge() {
+	    	return age;
+	      }
+
+	      public String getLevel() {
+		    return level;
+	      }
+
+	      public String getSpecializationCourse() {
+		    return specializationCourse;
+	      }
+	      
+	      
+	      public String print() {
+	    	  return "A talent -> "+name+ "," +fisrtName+ "," +age+ "," +level+ "," +specializationCourse;
+	      }
+}
